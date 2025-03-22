6b5b0382359cf37420753df36f904b6c49a9f531
Hernán Sánchez <105751698+HdSanch@users.noreply.github.com>
2025-03-22 10:59:39 -0500

    cambios

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,13 @@
- 
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // ✅ Importar el plugin de React
 import path from 'path';
 
 export default defineConfig({
+  plugins: [react()], // ✅ Ahora `react()` está definido
+  base: './', // Cambia a '/' si no se renderizan correctamente los assets
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
